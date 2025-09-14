febf23bee271238a34182bca58da460734d7fb13
Hafz-technology <khalilhafz@gmail.com>
2025-09-14 22:56:55 +0300

    task1

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Routes, Route, useParams } from 'react-router-dom';
-import { Router } from 'react-router-dom';
+// import { Router } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 
+
 function App() {
   return (
+     
     <div>
       <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
+      
+     
       <Routes>
+        <AddRecipeForm />
         <Route path="/" element={<RecipeList />} />
         <Route
           path="/recipes/:id"
           element={<RecipeDetailsWrapper />}
         />
       </Routes>
+     
     </div>
+    
+    
   );
 }
 
--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <div>Hello</div>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
+
+
+
+
+
+
+
