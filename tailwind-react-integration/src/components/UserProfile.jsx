8d66d33e67d7349fd93265c7f8208657a3c06967
Hafz-technology <khalilhafz@gmail.com>
2025-10-02 15:24:48 +0300

    React JS with a CSS Framework task1

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import devsecureLogo from '../assets/devsecure_logo.png'
 
 function UserProfile() {
   return (
-    <div className="user-profile">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg">
      <img src={devsecureLogo} alt="Logo" />
       <h1>Dev Secure Company</h1>
       <p>Dev Secure Technology and Training</p>
