ce0a7ed5b8290a357af1fa92465c03c40b8e0e5c
Hafz-technology <khalilhafz@gmail.com>
2025-10-07 14:38:14 +0300

    Implementing Form Handling in React with Controlled Components and Formik task2

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import ProtectedRoute from './components/ProtectedRoute'; 
 import Home from './pages/Home';
