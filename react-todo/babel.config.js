f318f83b8b79e06dc3706e3d9f486a5b301f5669
Hafz-technology <khalilhafz@gmail.com>
2025-10-07 20:29:41 +0300

    Routers, Data handling, Forms and Testing In React task3

---
+++ react-todo/babel.config.js
@@ -0,0 +1,6 @@
+export const presets = [
+    // Use for compiling modern JavaScript features
+    '@babel/preset-env',
+    // Use for transforming React JSX syntax
+    ['@babel/preset-react', { runtime: 'automatic' }]
+];
